bb90f0c823506492e4e486fd94e203dcc74ff6ec
renguoqing <1002674966@qq.com>
2018-05-02 14:00:23 +0800

    提交文件

--- src/main/java/com/gyc/trade/zipkin/ZipkinApplication.java
+++ src/main/java/com/gyc/trade/zipkin/ZipkinApplication.java
@@ -1,20 +1,30 @@
 package com.gyc.trade.zipkin;
 
+import java.util.Arrays;
+
 import javax.sql.DataSource;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import org.springframework.context.annotation.Bean;
 
-import zipkin.server.EnableZipkinServer;
 import zipkin.storage.mysql.MySQLStorage;
 
 @SpringBootApplication
-@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinApplication {
 
     public static void main(String[] args) {
-    	SpringApplication.run(ZipkinApplication.class, args);
+    	boolean hasProfile = Arrays.stream(args).anyMatch(arg -> arg.contains("configurl"));
+        String[] cArgs=args;
+        if(!hasProfile){
+            cArgs=new String[args.length+2];
+            cArgs[0]="--configurl=localhost";
+            cArgs[1]="--profile=local";
+            System.arraycopy(args,0,cArgs,2,args.length);
+        }
+    	new SpringApplicationBuilder(ZipkinApplication.class).web(true).run(args);
     }
     
     @Bean
